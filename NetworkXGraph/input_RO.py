62dbda43ec000cd78018fcc2a0bc4110cbff10cc
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-11-16 11:38:12 -0500

    RO generalization for replace

--- NetworkXGraph/input_RO.py
+++ NetworkXGraph/input_RO.py
@@ -50,26 +50,31 @@ def replace_operation(graph, id_ro, trigger, operation):
         operation (str): The operation object
     """
     existing_node = operation["existingNode"]
-    parent_nodes = list(graph.predecessors(existing_node))
-    current_existing_nodes_successors = list(graph.successors(existing_node))[0]
-    # Loop over all the parents of the existing node
-    # for parent_node in parent_nodes:
-    is_first_new_node = True
+
+    edge_to_successors = []
     for node in operation["newNodes"]:
         # taking node id and the rest of its attributes
         node_copy = {**node}
         new_node_id = node_copy["id"]
         node_type = node_copy["type"]
+
+        # if the current node needs to connect to the successors of the 'existing node'
+        if node_copy.get("edgeToSuccessors", False):
+            edge_to_successors.append(
+                {"node": new_node_id, **node_copy.get("edgeToSuccessorsAttr", {})}
+            )
+
+        # usually is 1 node
         predecessor_list = node_copy.get(
             "predecessors", list(graph.predecessors(existing_node))
         )
         if node_type == "action":
             node_copy["is_original"] = False
-        del node_copy["id"]
-        del node_copy["type"]
 
-        if "predecessors" in node_copy:
-            del node_copy["predecessors"]
+        node_copy.pop("id", None)
+        node_copy.pop("type", None)
+        node_copy.pop("predecessors", None)
+        node_copy.pop("edgeToSuccessors", None)
 
         # adding it to the graph
         # Currently assuming the added nodes are all actionNode
@@ -80,21 +85,21 @@ def replace_operation(graph, id_ro, trigger, operation):
             if isinstance(pred, str):
                 pred = {"nodeId": pred}
             pred_node = pred["nodeId"]
-            del pred["nodeId"]
+            pred.pop("nodeId", None)
             # copying the edge data if any but only to the first new node
             edge_data = graph.get_edge_data(pred_node, existing_node)
             edge_data = edge_data[0] if edge_data else {}
             if not graph.has_edge(pred_node, new_node_id):
                 graph.add_edge(pred_node, new_node_id, **pred, **edge_data)
 
-        # wont copy edge attributes further
-        is_first_new_node = False
-
-        # updating the parent node so the next new node has the correct edge
-        parent_node = new_node_id
-    if not graph.has_edge(pred_node, current_existing_nodes_successors):
-        # need one last edge from the last added node to the same node 'Existing_node' is pointing too
-        graph.add_edge(pred_node, current_existing_nodes_successors)
+    current_existing_nodes_successors = list(graph.successors(existing_node))
+    for edge in edge_to_successors:
+        node = edge["node"]
+        edge.pop("node", None)
+        for succ in current_existing_nodes_successors:
+            if not graph.has_edge(node, succ):
+                # need one last edge from the last added node to the same node 'Existing_node' is pointing too
+                graph.add_edge(node, succ, **edge)
 
 
 def delete_operation(graph, operation):
@@ -113,8 +118,6 @@ def delete_operation(graph, operation):
         for succ in successors:
             pred_edge_data = graph.get_edge_data(pred, node_to_delete)[0]
             succ_edge_data = graph.get_edge_data(node_to_delete, succ)[0]
-            print("pred: {}".format(pred) + str(type(pred_edge_data)))
-            print("succ: {}".format(succ) + str(type(succ_edge_data)))
 
             if not graph.has_edge(pred, succ):
                 graph.add_edge(pred, succ, **pred_edge_data, **succ_edge_data)
