e9cbce6e1ed7f261bca4075effa25bb54f281d5a
Jean-Luc Blais-Amyot <35350889+amyotjl@users.noreply.github.com>
2021-11-16 15:53:17 -0500

    2-step process

--- NetworkXGraph/input_RO.py
+++ NetworkXGraph/input_RO.py
@@ -39,18 +39,18 @@ def update_graph_with_ROs(graph, ros):
                 add_action(graph, id, trigger, op)
 
 
-def replace_operation(graph, id_ro, trigger, operation):
+def add_all_new_nodes(graph, id_ro, trigger, operation):
     """
-    Replace operation inserts a sequence of new nodes. The first node of the sequence is a sibling of the node to 'replace' with the same edge attributs to the predecessor.
+    Add a list of new nodes with some attributes. These nodes do not have any edges after the execution of this function.
+
+    Use 'add_all_new_edges' to add the respectives edges.
 
     Args:
         graph (networkx graph): The graph.
         id_ro (str): The ID of the revision operator.
         trigger (list): List of triggering nodes.
-        operation (str): The operation object
+        operation (object): The operation object.
     """
-    existing_node = operation["existingNode"]
-
     edge_to_successors = []
     for node in operation["newNodes"]:
         # taking node id and the rest of its attributes
@@ -64,10 +64,6 @@ def replace_operation(graph, id_ro, trigger, operation):
                 {"node": new_node_id, **node_copy.get("edgeToSuccessorsAttr", {})}
             )
 
-        # usually is 1 node
-        predecessor_list = node_copy.get(
-            "predecessors", list(graph.predecessors(existing_node))
-        )
         if node_type == "action":
             node_copy["is_original"] = False
 
@@ -81,6 +77,34 @@ def replace_operation(graph, id_ro, trigger, operation):
         graph.add_node(
             new_node_id, type=node_type, idRO=id_ro, trigger=trigger, **node_copy
         )
+    return edge_to_successors
+
+
+def add_all_new_edges(graph, operation, edge_to_successors):
+    """
+    Add all the edges from the operation object.
+
+    Nodes related to these edges must be added to the graph prior to this function call.
+
+    Use 'add_all_new_nodes' to add the respectives nodes before calling this function.
+
+
+    Args:
+        graph (networkx graph): The graph.
+        id_ro (str): The ID of the revision operator.
+        trigger (list): List of triggering nodes.
+        operation (object): The operation object.
+    """
+    existing_node = operation["existingNode"]
+
+    for node in operation["newNodes"]:
+        node_copy = {**node}
+        new_node_id = node_copy["id"]
+
+        predecessor_list = node_copy.get(
+            "predecessors", list(graph.predecessors(existing_node))
+        )
+
         for pred in predecessor_list:
             if isinstance(pred, str):
                 pred = {"nodeId": pred}
@@ -92,7 +116,8 @@ def replace_operation(graph, id_ro, trigger, operation):
             if not graph.has_edge(pred_node, new_node_id):
                 graph.add_edge(pred_node, new_node_id, **pred, **edge_data)
 
-    current_existing_nodes_successors = list(graph.successors(existing_node))
+            current_existing_nodes_successors = list(graph.successors(existing_node))
+
     for edge in edge_to_successors:
         node = edge["node"]
         edge.pop("node", None)
@@ -100,6 +125,24 @@ def replace_operation(graph, id_ro, trigger, operation):
             if not graph.has_edge(node, succ):
                 # need one last edge from the last added node to the same node 'Existing_node' is pointing too
                 graph.add_edge(node, succ, **edge)
+
+
+def replace_operation(graph, id_ro, trigger, operation):
+    """
+    Replace operation inserts a sequence of new nodes.
+
+    This function is a 2-steps process. First, we add all the nodes to be added. Secondly, we add all the edges.
+
+    This allows the RO file to have the nodes in any particular order.
+
+    Args:
+        graph (networkx graph): The graph.
+        id_ro (str): The ID of the revision operator.
+        trigger (list): List of triggering nodes.
+        operation (object): The operation object
+    """
+    edge_to_successors = add_all_new_nodes(graph, id_ro, trigger, operation)
+    add_all_new_edges(graph, operation, edge_to_successors)
 
 
 def delete_operation(graph, operation):
